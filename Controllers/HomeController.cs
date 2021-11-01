b7720234b7ce2c8fe3158908f60541cc4acf02cb
nathan <ennemmjones@gmail.com>
2021-11-01 17:34:11 -0400

    to be graded

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Assessment5a.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Assessment5a.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Calculator()
+        {
+            
+            return View("Calculator");
+        }
+        public IActionResult Result([Bind("num1", "num2", "operation")] Calculator newCalc)
+        //public IActionResult Result(double num1, double num2, string operation)
+        {
+            double result = 0;
+            switch (newCalc.operation)
+            {
+                
+                case "Plus":
+                   result = newCalc.num1 + newCalc.num2;
+                break;
+                case "Minus":
+                   result = newCalc.num1 - newCalc.num2;
+                break;
+                case "Multiply":
+                    result = newCalc.num1 * newCalc.num2;
+                    break;
+                case "Divide":
+                    result = newCalc.num1 / newCalc.num2;
+                    break;
+
+            default:
+                    break;
+        }
+
+        
+            ViewBag.Result = result;
+            ViewBag.Operation = newCalc.operation;
+            
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assessment5a.Models
+{
+    public class Calculator
+    {
+        public double num1 { get; set; }
+        public double num2 { get; set; }
+        public string operation { get; set; }
+
+        
+    }
+}
